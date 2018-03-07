0d8572886dc54d208044e100721df40700afcae8
tega23 <tega23@yahoo.com>
2018-03-07 16:04:26 +0100

    Added pagination feature to the pages

--- LocalLibrary/catalog/views.py
+++ LocalLibrary/catalog/views.py
@@ -22,13 +22,10 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
-    
+    paginate_by = 10
     context_object_name = 'book_list'
     template_name = 'book_list.html'
-    """py
-    def get_queryset(self):
-        return Book.objects.filter(title__icontains = 'war' )[:5]
-    """
+    
     def get_context_data(self , **kwargs):
         context = super(BookListView , self).get_context_data(**kwargs)
         context['some_data']= 'This is just some data'
